0b198418de4e918f16cab22d3b5f6981cdd544e9
Roman Zanko <rzanko@aras.com>
2020-09-16 20:27:46 +0300

    Fix formatting

--- String array duplicates/Solution.cs
+++ String array duplicates/Solution.cs
@@ -1,35 +1,28 @@
 using System;
-using System.Text;
 using System.Linq;
-public class Solution
-{
-    public static string[] dup(string[] arr)
-    {
-        if(arr == null)
-        {
-            throw new ArgumentException();
-        }
-		return arr.Select(item=>DeleteDublicate(item)).ToArray();
-    }
+using System.Text;
+public class Solution {
+	public static string[] dup (string[] arr) {
+		if (arr == null) {
+			throw new ArgumentException ();
+		}
+		return arr.Select (item => DeleteDublicate (item)).ToArray ();
+	}
 
-	private static string DeleteDublicate(string item)
-	{
-		if(string.IsNullOrEmpty(item))
-        {
-            throw new ArgumentException("String is null or empty");
-        }
+	private static string DeleteDublicate (string item) {
+		if (string.IsNullOrEmpty (item)) {
+			throw new ArgumentException ("String is null or empty");
+		}
 
-        StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.Append(item[0]);
-		
-        for (int i = 1; i < item.Length; i++)
-        {
-			if(item[i] != item[i-1])
-			{
-				stringBuilder.Append(item[i]);
+		StringBuilder stringBuilder = new StringBuilder ();
+		stringBuilder.Append (item[0]);
+
+		for (int i = 1; i < item.Length; i++) {
+			if (item[i] != item[i - 1]) {
+				stringBuilder.Append (item[i]);
 			}
-        }
+		}
 
-		return stringBuilder.ToString();
+		return stringBuilder.ToString ();
 	}
 }
