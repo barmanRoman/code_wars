e212d174dca09d04c01ed79b7320702be34d4c9c
Roman Zanko <rzanko@aras.com>
2020-09-16 20:26:02 +0300

    Add solution of "String array duplicates" problem

--- String array duplicates/Solution.cs
+++ String array duplicates/Solution.cs
@@ -1,7 +1,35 @@
+using System;
+using System.Text;
+using System.Linq;
 public class Solution
 {
     public static string[] dup(string[] arr)
     {
-        return null;    
+        if(arr == null)
+        {
+            throw new ArgumentException();
+        }
+		return arr.Select(item=>DeleteDublicate(item)).ToArray();
     }
+
+	private static string DeleteDublicate(string item)
+	{
+		if(string.IsNullOrEmpty(item))
+        {
+            throw new ArgumentException("String is null or empty");
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.Append(item[0]);
+		
+        for (int i = 1; i < item.Length; i++)
+        {
+			if(item[i] != item[i-1])
+			{
+				stringBuilder.Append(item[i]);
+			}
+        }
+
+		return stringBuilder.ToString();
+	}
 }
